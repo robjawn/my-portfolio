5818f190bdb45d6707844faa43a806454199d5f3
Robert Johnson <robljohn1998@gmail.com>
2023-02-16 23:20:09 -0500

    wip on data

--- src/data.js
+++ src/data.js
@@ -1,34 +1,34 @@
 export const projects = [
     {
-      title: "React Reserve",
+      title: "ChronoCollage",
+      subtitle: "Django",
+      description:
+        "A historical photography app built with Django utilizing AWS S3 and bit.io for database handling.",
+      image: "https://camo.githubusercontent.com/03224d934908076d0cfd8d0184cea503d89e8c97ceb8bb1d794169158430da1d/68747470733a2f2f692e696d6775722e636f6d2f77487059674b312e706e67",
+      link: "https://chronocollage.herokuapp.com/",
+    },
+    {
+      title: "Mercantile",
       subtitle: "MERN Stack",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+        "A full-stack e-commerce application collaboratively built by a four person team utilizing Google Firebase.",
+      image: "https://camo.githubusercontent.com/a17c65efe173c4c3c34c03b2e3c137abf76d74b81f8b4f21f4012066bcc8740a/68747470733a2f2f692e696d6775722e636f6d2f6a424251316c622e6a7067",
+      link: "https://m3rcantile.netlify.app/",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "Myth Wiki",
+      subtitle: "Node.js, Express, MongoDB",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
-    },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
+        "A work in progress wiki for ancient mythology, built with Express and MongoDB.",
+      image: "https://camo.githubusercontent.com/e9bc6cd60fd2a99629ff745fb7eac8e474264803c20ed1d19dec454dd088fa5d/68747470733a2f2f692e696d6775722e636f6d2f584e77736b7a582e6a7067",
       link: "https://jsbootcamp.com",
     },
     {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
+      title: "Call Me Maybe",
+      subtitle: "JQuery",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-4.gif",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum?",
+      image: "https://camo.githubusercontent.com/277cbb6936938e3dbf612d43a90aebdcc91283dacdee021ec041b166f72f230e/68747470733a2f2f692e696d6775722e636f6d2f6c683551706b452e706e67",
       link: "https://pythonbootcamp.com",
     },
   ];
